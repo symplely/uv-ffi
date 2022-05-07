8e45e9b72f34b41bbf70092430a0ba0ba64027e8
L. Stubbs <lstubbs@techno.express>
2022-05-06 23:58:39 -0400

    remove phpunit, mark as project for easer install

--- .ignore.php
+++ .ignore.php
@@ -2,24 +2,24 @@
 
 declare(strict_types=1);
 
-if (\file_exists('.gitignore')) {
-  $ignore = \file_get_contents('.gitignore');
+if (\file_exists('../' . '.gitignore')) {
+  $ignore = \file_get_contents('../' . '.gitignore');
   if (\strpos($ignore, '.cdef/') === false) {
     $ignore .= '.cdef/' . \PHP_EOL;
-    \file_put_contents('.gitignore', $ignore);
+    \file_put_contents('../' . '.gitignore', $ignore);
   }
 } else {
-  \file_put_contents('.gitignore', '.cdef/' . \PHP_EOL);
+  \file_put_contents('../' . '.gitignore', '.cdef/' . \PHP_EOL);
 }
 echo "- Initialized .gitignore" . PHP_EOL;
 
-if (\file_exists('.gitattributes')) {
-  $export = \file_get_contents('.gitattributes');
+if (\file_exists('../' . '.gitattributes')) {
+  $export = \file_get_contents('../' . '.gitattributes');
   if (\strpos($export, '/.cdef') === false) {
     $export .= '/.cdef       export-ignore' . \PHP_EOL;
-    \file_put_contents('.gitattributes', $export);
+    \file_put_contents('../' . '.gitattributes', $export);
   }
 } else {
-  \file_put_contents('.gitattributes', '/.cdef       export-ignore' . \PHP_EOL);
+  \file_put_contents('../' . '.gitattributes', '/.cdef       export-ignore' . \PHP_EOL);
 }
 echo "- Initialized .gitattributes" . PHP_EOL;
